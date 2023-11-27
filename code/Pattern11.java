559c56251fd32170b741c728999206f580ce6fbb
abhay <abhayagarwal4483@gmail.com>
2023-11-27 15:08:30 +0530

    Java Patterns Printing code

---
+++ code/Pattern11.java
@@ -0,0 +1,21 @@
+public class Pattern11{
+
+    public static void main(String[] args) {
+        int row=5;
+        int space=row-1;
+        int star=1;
+        for(int i=1;i<=row;i++){
+            int value=i;
+            for(int j=0;j<space;j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<star;j++){
+                System.out.print(value+" ");
+            }
+            space--;
+            star++;
+            System.out.println();
+        }
+        
+    }
+}
