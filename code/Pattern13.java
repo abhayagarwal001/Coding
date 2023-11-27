9544ad2685be5180f26779aa46bc0d35e1fa7bfc
abhay <abhayagarwal4483@gmail.com>
2023-11-27 23:36:49 +0530

    Java Patterns Printing code

---
+++ code/Pattern13.java
@@ -0,0 +1,46 @@
+public class Pattern13 {
+    public static void main(String[] args) {
+        int row=4;
+        int space=row*2-2;
+        int star=1;
+        
+        //upper
+        for(int i=0;i<row;i++){
+            for(int j=0;j<star;j++){
+                System.out.print("* ");
+            }
+            for(int j=0;j<space;j++)
+            {
+                System.out.print("  ");
+            }
+            for(int j=0;j<star;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+            space=space-2;
+            star=star+1;
+       }
+
+       //lower
+       space=0;
+       star=row;
+       for(int i=0;i<row;i++){
+            for(int j=0;j<star;j++){
+                System.out.print("* ");
+            }
+            for(int j=0;j<space;j++)
+            {
+                System.out.print("  ");
+            }
+            for(int j=0;j<star;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+            space=space+2;
+            star=star-1;
+       }
+
+
+
+    }
+}
