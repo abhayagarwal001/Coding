88ecf1fd62941e1718e0be9730a7c7e23e39701b
abhay <abhayagarwal4483@gmail.com>
2023-11-28 13:41:42 +0530

    Java Patterns Printing code

---
+++ code/Pattern16.java
@@ -0,0 +1,17 @@
+public class Pattern16 {
+    public static void main(String[] args) {
+        int row=5;
+        int space=4;
+        for(int i=0;i<row;i++){
+        for(int j=0;j<space;j++){
+            System.out.print("  ");
+        }
+        for(int j=0;j<row;j++){
+            System.out.print("* ");
+        }
+        space--;
+        System.out.println();
+        }
+
+    }
+}
